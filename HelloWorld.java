aad06ca5721258dd441080d4996d32982b16a54b
ViswanathMU <viswanath.mu@gmail.com>
2022-02-26 18:53:38 +0530

    First

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/* 
+  This is a simple class called 
+  HelloWorld in a file HelloWorld.java 
+*/
+class HelloWorld
+{
+    //This program starts with a call to "main" method
+    public static void main(String args[])
+    {
+        //The below line prints a greeting on the console
+        System.out.println("Hello! I'm a Try QA program");
+    }
+}
